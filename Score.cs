6568ddbead55e6fd0ff17ffc3e056ce5cd94d9bc
StarDestroyer66 <72447658+SavuStefan@users.noreply.github.com>
2024-06-05 12:18:25 +0300

    added files for testing

---
+++ Score.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Design_Patterns___DOOM_Game;
+
+
+public class Scoreboard
+{
+    private static Scoreboard _instance;
+    private int _score;
+
+    private Scoreboard()
+    {
+        _score = 0;
+    }
+
+    public static Scoreboard Instance
+    {
+        get
+        {
+            if (_instance == null)
+                _instance = new Scoreboard();
+            return _instance;
+        }
+    }
+
+    public int GetScore()
+    {
+        return _score;
+    }
+
+    public void AddScore(int points)
+    {
+        _score += points;
+    }
+
+    public void ResetScore()
+    {
+        _score = 0;
+    }
+}
+
---
+++ WeaponsFactory.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+
+namespace Design_Patterns___DOOM_Game
+{
+    public abstract class Weapon
+    {
+        public abstract void Use();
+    }
+
+    public class Chainsaw : Weapon
+    {
+        public override void Use()
+        {
+            Console.WriteLine("Chainsaw found!");
+        }
+    }
+
+ 
+    abstract class WeaponFactory
+    {
+        public abstract Weapon CreateWeapon();
+    }
+
+    class ChainsawFactory : WeaponFactory
+    {
+        public override Weapon CreateWeapon()
+        {
+            return new Chainsaw();
+        }
+    }
+
+
+}
